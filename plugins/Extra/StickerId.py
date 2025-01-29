828baf2e30b8e3c7a4c341f5c5f467b3df84c0a1
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2025-01-30 01:59:13 +0530

    Update StickerId.py

--- plugins/Extra/StickerId.py
+++ plugins/Extra/StickerId.py
@@ -4,7 +4,7 @@ import logging
 import pyrogram
 import time
 import math
-from info import *
+from info import *, BOT_TOKEN
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import User, Message, Sticker, Document
